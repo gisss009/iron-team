9afc2cd9639a3e5fc11f4b44875dbe4dbd69fc23
Виктория Горбанева <viktoryagorbaneva@yandex.ru>
2024-10-04 14:51:04 +0300

    GNOME SORT

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iron_project
+{
+    internal class Class1
+    {
+        //перестановка местами двух элементов в массиве
+        static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        public static void GnomeSort(int[] inArray)
+        {
+            int i = 1;
+            int j = 2;
+            while (i < inArray.Length)
+            {
+                if (inArray[i - 1] < inArray[i])
+                {
+                    i = j;
+                    j += 1;
+                }
+                else
+                {
+                    Swap(inArray, i - 1, i);
+                    i -= 1;
+                    if (i == 0)
+                    {
+                        i = j;
+                        j += 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace iron_project
+
+
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Введите через запятую целые числа и нажмите Enter");
+            string[] nums = Console.ReadLine().Split(new char[] { ',' });
+            int[] intArray = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                intArray[i] = int.Parse(nums[i]);
+            }
+
+            Class1.GnomeSort(intArray);
+
+            Console.WriteLine("\r\nОтсортированный массив:");
+            foreach (int value in intArray)
+            {
+                Console.Write($"{value} ");
+            }
+        }
+    }
+}
